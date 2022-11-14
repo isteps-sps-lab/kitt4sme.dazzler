c099481a46299ad118db948bcfb55554d6011742
Andrea Falconi <andrea.falconi@gmail.com>
2022-11-14 20:01:34 +0100

    hook real insight board into demo.

--- tests/sim/runner.py
+++ tests/sim/runner.py
@@ -1,10 +1,12 @@
 from time import sleep
 
 from fipy.docker import DockerCompose
+from fipy.ngsi.orion import OrionClient
 from fipy.ngsi.quantumleap import QuantumLeapClient
 
 from tests.util.fiware import quantumleap_client, \
-    inspection_demo_batches_stream, roughness_estimate_batches_stream
+    inspection_demo_batches_stream, roughness_estimate_batches_stream, \
+    orion_client, insight_demo_batches_stream
 
 
 docker = DockerCompose(__file__)
@@ -15,8 +17,11 @@ def bootstrap():
     docker.start()
 
 
-def send_entities(quantumleap: QuantumLeapClient):
+def send_entities(quantumleap: QuantumLeapClient, orion: OrionClient):
     try:
+        batch = next(insight_demo_batches_stream)
+        orion.upsert_entities(batch)
+
         batch = next(roughness_estimate_batches_stream) + \
                 next(inspection_demo_batches_stream)
         quantumleap.insert_entities(batch)
@@ -27,15 +32,16 @@ def send_entities(quantumleap: QuantumLeapClient):
 def run():
     services_running = False
     quantumleap = quantumleap_client()
+    orion = orion_client()
     sampling_rate = 5.0
     try:
         bootstrap()
         services_running = True
 
-        print('>>> sending entities to Quantum Leap...')
+        print('>>> sending entities to Quantum Leap and Orion...')
         while True:
             sleep(sampling_rate)
-            send_entities(quantumleap)
+            send_entities(quantumleap, orion)
 
     except KeyboardInterrupt:
         if services_running:
