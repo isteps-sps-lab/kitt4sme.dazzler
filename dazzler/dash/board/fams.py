9077e605f37ab8b82802bc93f9c52cd314bb83ed
Vincenzo Cutrona <vincenzo.cutrona@supsi.ch>
2023-05-09 12:58:59 +0200

    display interventions from IM

--- dazzler/dash/board/fams.py
+++ dazzler/dash/board/fams.py
@@ -1,5 +1,6 @@
+import datetime
 from abc import ABC
-from typing import Tuple
+from typing import Tuple, Dict
 
 import dash_bootstrap_components as dbc
 import numpy as np
@@ -36,13 +37,17 @@ class FatigueDashboard(ABC):
     def _build_layout(self):
         self.app.layout = dbc.Container(
             [
-                html.H1("Worker cells fatigue monitoring dashboard"),
+                html.H1("Worker lines fatigue monitoring dashboard"),
                 dbc.Row(
                     [
                         dbc.Col(
-                            dcc.Markdown(
-                                '''This graph shows the current levels of **fatigue** for each worker assigned to the
-                                related production cell.'''
+                            html.P(
+                                [
+                                    'This graph shows the current levels of ',
+                                    html.Strong('fatigue'),
+                                    ' for each worker assigned to the related production line.'
+                                ],
+                                className='lead'
                             ),
                             md=12),
                         html.Hr(),
@@ -56,6 +61,17 @@ class FatigueDashboard(ABC):
                             id='worker-interval',
                             interval=5 * 1000,  # in milliseconds
                             n_intervals=0
+                        ),
+
+                        dbc.Col(
+                            children=[self._build_interventions()],
+                            md=12,
+                            id="interventions"
+                        ),
+                        dcc.Interval(
+                            id='interventions-interval',
+                            interval=5 * 1000,  # in milliseconds
+                            n_intervals=0
                         )
                     ]
                 ),
@@ -67,7 +83,7 @@ class FatigueDashboard(ABC):
         tix = pd.Timestamp.now('utc') - pd.Timedelta(minutes=minutes - 1)
         return pd.date_range(tix, periods=minutes, freq="T")
 
-    def _worker_cell(self, worker_id):
+    def _worker_line(self, worker_id):
         return ord(worker_id[-1]) % 3
 
     def _fetch_workers_data(self) -> Tuple[pd.DataFrame, pd.DataFrame]:
@@ -81,7 +97,7 @@ class FatigueDashboard(ABC):
             worker_data = {
                 k: r[k].set_index('index').workerStates.apply(
                     lambda x: x["fatigue"]["level"]["value"] if x else x).resample('T').mean().to_frame(
-                    name=f'Cell{self._worker_cell(k) + 1}'
+                    name=f'Line{self._worker_line(k) + 1}'
                 )
                 for k in r if r[k]['workerStates'].any()}
 
@@ -92,28 +108,28 @@ class FatigueDashboard(ABC):
         except (HTTPError, ConnectionError) as e:
             print(f"No data available for the given time window {dti[0]} -- {dti[-1]}")
             print(e)
-            return pd.DataFrame(columns=['workers', 'cell']), self._empty_dataset()
+            return pd.DataFrame(columns=['workers', 'line']), self._empty_dataset()
 
-        workers_cells_df = pd.DataFrame(
+        workers_lines_df = pd.DataFrame(
             {'workers': worker_data.keys(),
-             'cell': [f'Cell{self._worker_cell(w_id) + 1}' for w_id in worker_data]}
-        ).groupby('cell').count()
+             'line': [f'Line{self._worker_line(w_id) + 1}' for w_id in worker_data]}
+        ).groupby('line').count()
 
-        return workers_cells_df, fatigue_df.groupby(fatigue_df.index).mean()
+        return workers_lines_df, fatigue_df.groupby(fatigue_df.index).mean()
 
     def _empty_dataset(self, dti=None) -> pd.DataFrame:
         if dti is None:
             dti = self._date_time_index_utc()
         df = pd.DataFrame({
-            'Cell1': [np.nan] * len(dti),
-            'Cell2': [np.nan] * len(dti),
-            'Cell3': [np.nan] * len(dti)
+            'Line1': [np.nan] * len(dti),
+            'Line2': [np.nan] * len(dti),
+            'Line3': [np.nan] * len(dti)
         }, index=dti)
 
         return df.resample('T').mean()
 
     def _build_worker_graphs(self, n=0) -> Component:
-        workers_by_cell_df, worker_data = self._fetch_workers_data()
+        workers_by_line_df, worker_data = self._fetch_workers_data()
         worker_data.index = worker_data.index.tz_convert('CET')  # read timezone from env
 
         return dbc.Row(
@@ -121,7 +137,7 @@ class FatigueDashboard(ABC):
                 dbc.Col(
                     [
                         html.Center([
-                            html.H2(f'Connected workers: {workers_by_cell_df["workers"].sum()}'),
+                            html.H2(f'Connected workers: {workers_by_line_df["workers"].sum()}'),
                         ]),
                     ],
                     md=12
@@ -129,16 +145,16 @@ class FatigueDashboard(ABC):
                 dbc.Col(
                     [
                         html.Center([
-                            html.H4(f'% of workers per workcell'),
+                            html.H4(f'% of workers per line'),
                         ]),
                         dbc.Col(
-                            dcc.Graph(id="workers-by-cell",
+                            dcc.Graph(id="workers-by-line",
                                       figure=px.pie(
-                                          workers_by_cell_df,
+                                          workers_by_line_df,
                                           title="",
                                           values="workers",
-                                          names=workers_by_cell_df.index,
-                                          color=workers_by_cell_df.index,
+                                          names=workers_by_line_df.index,
+                                          color=workers_by_line_df.index,
                                           color_discrete_sequence=['rgb(248,156,116)', 'rgb(139,224,164)',
                                                                    'rgb(158,185,243)'],
                                       ))
@@ -149,11 +165,11 @@ class FatigueDashboard(ABC):
                 dbc.Col(
                     [
                         html.Center([
-                            html.H4("Current fatigue per workcell"),
+                            html.H4("Current fatigue per line"),
                         ]),
                         dbc.Col(
                             dcc.Graph(id="current-fatigue",
-                                      figure=self._build_worker_cell_fatigue_last(worker_data)),
+                                      figure=self._build_worker_line_fatigue_last(worker_data)),
                             # width={"size": 8, "offset": 2},
                         ),
                     ],
@@ -163,11 +179,11 @@ class FatigueDashboard(ABC):
                 dbc.Col(
                     [
                         html.Center([
-                            html.H4("Historical fatigue per workcell"),
+                            html.H4("Historical fatigue per line"),
                         ]),
                         dbc.Col(
                             dcc.Graph(id="timeseries-fatigue",
-                                      figure=self._build_worker_cell_fatigue_timeseries(worker_data)),
+                                      figure=self._build_worker_line_fatigue_timeseries(worker_data)),
                             # width={"size": 8, "offset": 2},
                         ),
                     ],
@@ -177,27 +193,89 @@ class FatigueDashboard(ABC):
             ],
         )
 
-    def _build_worker_cell_fatigue_last(self, fatigue_df):
+    def _build_worker_line_fatigue_last(self, fatigue_df):
         return px.bar(
             # title='Control',
             x=fatigue_df.columns,
-            y=[fatigue_df[cell].mean() for cell in fatigue_df.columns],
+            y=[fatigue_df[line].mean() for line in fatigue_df.columns],
             error_y=[fatigue_df[col].std() for col in fatigue_df.columns],
-            labels={'x': 'Cell', 'y': 'Fatigue [avg]', 'color': 'Legend'},
+            labels={'x': 'Line', 'y': 'Fatigue [avg]', 'color': 'Legend'},
             color=fatigue_df.columns,
             color_discrete_sequence=['rgb(248,156,116)', 'rgb(139,224,164)', 'rgb(158,185,243)'],
             range_y=[0, 10]
         )
 
-    def _build_worker_cell_fatigue_timeseries(self, fatigue_df):
+    def _build_worker_line_fatigue_timeseries(self, fatigue_df):
         return px.line(
             fatigue_df,
             labels={'index': 'Time', 'value': 'Fatigue [avg]', 'variable': 'Legend'},
             color_discrete_sequence=['rgb(248,156,116)', 'rgb(139,224,164)', 'rgb(158,185,243)'],
             range_y=[0, 10])
 
+    def _fetch_intervention(self) -> Dict:
+        try:
+            from_ = pd.Timestamp.now('utc') - pd.Timedelta(hours=1)
+            assignment = list(self._quantumleap.fetch_entity_type_series(entity_type="TaskAssignment",
+                                                                         from_timepoint=from_).values())[0].to_dict(
+                orient='records')[-1]
+            execution = list(self._quantumleap.fetch_entity_type_series(entity_type="TaskExecution",
+                                                                        from_timepoint=from_).values())[0].to_dict(
+                orient='records')[-1]
+            if int(assignment['creationTimestamp']) > int(execution['creationTimestamp']):
+                return {
+                    'datetime': datetime.datetime.fromtimestamp(int(assignment['creationTimestamp']) / 1000),
+                    'intervention': "Reconfigure",
+                    "from": int(assignment['oldTask'][-1]),
+                    "to": int(assignment['newTask'][-1]),
+                    "workers": assignment['additionalParameters']['numberOfWorkers']
+                }
+
+            else:
+                return {
+                    'datetime': datetime.datetime.fromtimestamp(int(execution['creationTimestamp']) / 1000),
+                    'intervention': "Continue"
+                }
+
+        except Exception as e:
+            return {}
+
+    def _build_interventions(self, n=0) -> Component:
+        intervention = self._fetch_intervention()
+        if not intervention:
+            p = [html.Small(datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S"), className="text-muted"),
+                 ' No interventions']
+        else:
+            p = [html.Small(intervention['datetime'].strftime("%d-%m-%Y %H:%M:%S"), className="text-muted")]
+            if intervention['intervention'] == 'Reconfigure':
+                p += [
+                    ' Move ',
+                    html.Strong(intervention['workers']),
+                    ' workers from ',
+                    html.Strong(f'Line{intervention["from"]}'),
+                    ' to ',
+                    html.Strong(f'Line{intervention["to"]}'),
+                ]
+            else:
+                p += ['Keep the current configuration']
+
+        return dbc.Row([
+            dbc.Col(
+                [
+                    html.H2(f'Last accepted intervention'),
+                    html.P(p, className='lead')
+                ],
+                md=12,
+                className='gy-4'
+            ),
+        ])
+
     def _build_callbacks(self):
         self.app.callback(
             Output("worker_graphs", 'children'),
             Input('worker-interval', 'n_intervals')
         )(self._build_worker_graphs)
+
+        self.app.callback(
+            Output("interventions", 'children'),
+            Input('interventions-interval', 'n_intervals')
+        )(self._build_interventions)
